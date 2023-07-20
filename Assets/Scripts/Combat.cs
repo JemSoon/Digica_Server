a9a403da8caeb38d276e0344c03edc296ea549c3
JemSoon <97009299+JemSoon@users.noreply.github.com>
2023-07-21 03:22:29 +0900

    방장은 메모리체커 카드내면 움직임

--- Assets/Scripts/Combat.cs
+++ Assets/Scripts/Combat.cs
@@ -10,7 +10,11 @@ public class Combat : NetworkBehaviour
     public void CmdChangeMana(int amount)
     {
         // Increase mana by amount. If 3, increase by 3. If -3, reduce by 3.
-        if (entity is Player) entity.GetComponent<Player>().mana += amount;
+        if (entity is Player) 
+        { 
+            entity.GetComponent<Player>().mana += amount;
+            MemoryChecker.Inst.memoryCheckerPos(); // 카드를 냄에 따른 메모리 변화
+        }
     }
 
     [Command]
@@ -25,7 +29,7 @@ public class Combat : NetworkBehaviour
     {
         // Increase health by amount. If 3, increase by 3. If -3, reduce by 3.
         entity.health += amount;
-        if (entity.health <= 0) Destroy(entity.gameObject);
+        //if (entity.health <= 0) Destroy(entity.gameObject);
     }
 
     [Command]
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -103,8 +103,6 @@ public class GameManager : NetworkBehaviour
     {
         endTurnButton.SetActive(true);
         Player player = Player.localPlayer;
-        player.mana++;
-        player.currentMax++;
         isOurTurn = true;
     }
 }

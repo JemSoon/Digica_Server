c0387121841dfa0715fedfe3426978405a15c719
JemSoon <97009299+JemSoon@users.noreply.github.com>
2023-09-05 08:43:16 +0900

    야매 하드코딩 세큐리티 아이콘 상태 업데이트

---
+++ Assets/Scripts/UI/MySecurityCount.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MySecurityCount : MonoBehaviour
+{
+    public Sprite normalImage;
+    public Sprite breakImage;
+
+    public Image Icon1;
+    public Image Icon2;
+    public Image Icon3;
+    public Image Icon4;
+    public Image Icon5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetIcon();
+    }
+
+    private int SecurityCountCheck()
+    {
+        if(Player.localPlayer != null)
+        {
+            return Player.localPlayer.deck.securityCard.Count;
+        }
+
+        else { return 0; }
+    }
+
+    private void SetIcon()
+    {
+        switch (SecurityCountCheck())
+        {
+            case 0:
+                Icon1.sprite = breakImage;
+                Icon2.sprite = breakImage;
+                Icon3.sprite = breakImage;
+                Icon4.sprite = breakImage;
+                Icon5.sprite = breakImage;
+                break;
+            case 1:
+                Icon1.sprite = breakImage;
+                Icon2.sprite = breakImage;
+                Icon3.sprite = breakImage;
+                Icon4.sprite = breakImage;
+                Icon5.sprite = normalImage;
+                break;
+            case 2:
+                Icon1.sprite = breakImage;
+                Icon2.sprite = breakImage;
+                Icon3.sprite = breakImage;
+                Icon4.sprite = normalImage;
+                Icon5.sprite = normalImage;
+                break;
+            case 3:
+                Icon1.sprite = breakImage;
+                Icon2.sprite = breakImage;
+                Icon3.sprite = normalImage;
+                Icon4.sprite = normalImage;
+                Icon5.sprite = normalImage;
+                break;
+            case 4:
+                Icon1.sprite = breakImage;
+                Icon2.sprite = normalImage;
+                Icon3.sprite = normalImage;
+                Icon4.sprite = normalImage;
+                Icon5.sprite = normalImage;
+                break;
+            case 5:
+                Icon1.sprite = normalImage;
+                Icon2.sprite = normalImage;
+                Icon3.sprite = normalImage;
+                Icon4.sprite = normalImage;
+                Icon5.sprite = normalImage;
+                break;
+        }
+    }
+}

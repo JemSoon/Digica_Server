314ff4d46a3b0d74a8933a7377f1f5a36e737102
JemSoon <97009299+JemSoon@users.noreply.github.com>
2023-11-14 04:10:28 +0900

    진화카드 올릴때 상대 플레이어 카드에 올리게 되는 문제 수정
    조건문에 플레이어 확인 코드 추가

--- Assets/Scripts/UI/HandCardDragHover.cs
+++ Assets/Scripts/UI/HandCardDragHover.cs
@@ -107,7 +107,8 @@ public class HandCardDragHover : MonoBehaviour, IBeginDragHandler, IDragHandler,
         if (card.cardInfo.data is CreatureCard handCard && // hand카드가 CreatureCard이고
             collision.GetComponent<FieldCard>().card.data is CreatureCard fieldCard && // field카드가 CreatureCard이고
             isDragging == true && // 드래그 중이고
-            collision.GetComponent<FieldCard>().upperCard==null) //아직 upperCard가 없다면
+            collision.GetComponent<FieldCard>().upperCard==null && //아직 upperCard가 없다면
+            collision.GetComponent<FieldCard>().player == Player.localPlayer) //내 카드라면
         {
             //만약 ScriptableCard의 종류가 Creature카드라면 레벨 정보 + 색상을 가져온다
             if (handCard.level == fieldCard.level + 1 &&
